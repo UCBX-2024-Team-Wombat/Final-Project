6434a994f63f52833f5e12f17d66dcf71fc8c426
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-11-30 12:09:37 -0800

    Add media queries to initial state

--- client/src/utils/GlobalState.jsx
+++ client/src/utils/GlobalState.jsx
@@ -1,11 +1,22 @@
 import { createContext, useContext, useReducer } from "react";
+import { useMediaQuery } from "react-responsive";
 import reducer from "./reducers";
 
 const MainContext = createContext();
 const { Provider } = MainContext;
 
 const MainProvider = ({ value = [], ...props }) => {
-  const [state, dispatch] = useReducer(reducer, {});
+  const isMobile = useMediaQuery({ query: "(max-width: 500px)" });
+  const isTablet = useMediaQuery({ query: "(max-width: 1000px)" });
+  const isDesktop = useMediaQuery({ query: "(min-width: 1000px)" });
+
+  const initialState = {
+    isMobile: isMobile,
+    isTablet: isTablet,
+    isDesktop: isDesktop,
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return <Provider value={[state, dispatch]} {...props} />;
 };
