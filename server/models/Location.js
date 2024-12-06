9aa301c31b9abf5d017c17e99add41e891fd48a9
yaseminvatan <66827861+yaseminvatan@users.noreply.github.com>
2024-12-05 19:47:02 -0800

    Add city, province, and state fields to schema with city as required and optional province/state fields

--- server/models/Location.js
+++ server/models/Location.js
@@ -1,7 +1,23 @@
 const mongoose = require("mongoose");
 const { Schema } = require("mongoose");
 
-const locationSchema = new Schema({});
+const locationSchema = new Schema({
+    city: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      province: {
+        type: String,
+        required: false,
+        trim: true,
+      },
+      state: {
+        type: String,
+        required: false,
+        trim: true,
+      },
+});
 
 const Location = mongoose.model("Location", locationSchema);
 
