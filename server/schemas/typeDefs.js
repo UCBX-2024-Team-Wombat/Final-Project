f82788940d35f71016806f849328bd5e07454e55
Bradyn Wolkoys <bnwolkoys@gmail.com>
2024-12-05 20:07:12 -0800

    added skills and skillsrelation to typeDefs, also added modify and delete skill to mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,4 @@
-const typeDefs = `
+const typeDefs = gql`
   type User {
     _id: ID
     username: String
@@ -18,18 +18,29 @@ const typeDefs = `
   }
 
   type SkillRelationship {
-  
+    skill: Skill
+    yearsOfExperience: Int
+    areasOfExpertise: String
+    user: [User]
   }
 
   type Query {
     user(id: ID!): User
     me: User
+    skills: [Skill]
+    skill(id: ID!): Skill
+    getSkillRelationships: [SkillRelationship]
   }
 
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
+    addSkill(name: String!, description: String!): Skill
+    modifySkill(id: ID!, name: String!, description: String!): Skill
+    deleteSkill(id: ID!): Skill
+    addSkillRelationship(skill: ID!, yearsOfExperience: Int!, areasOfExpertise: String!): SkillRelationship
   }
 `;
+//mutations: ability to add, modify and delete a skill
 
 module.exports = typeDefs;
