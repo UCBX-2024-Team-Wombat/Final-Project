bfa5aa5245eaec3753e2167ba6f7bb647ef3b8ef
Bradyn Wolkoys <bnwolkoys@gmail.com>
2024-12-05 19:29:57 -0800

    Feature/skill model (#6)
    * created skill schema model and input name and description fields
    
    * added varaibles- name and description and a reference
    
    * changed requirred to false for description

--- server/models/Skill.js
+++ server/models/Skill.js
@@ -1,7 +1,18 @@
 const mongoose = require("mongoose");
 const { Schema } = require("mongoose");
 
-const skillSchema = new Schema({});
+const skillSchema = new Schema(
+    {
+        //help from module 21, activity 5 
+    name: {
+        type: string,
+        required: true
+    },
+    description: {
+        type: string,
+        required: false
+    }
+});
 
 const Skill = mongoose.model("Skill", skillSchema);
 
