d2bc1d4c5ed8cb31d716f22b276be434a361eda8
Bradyn Wolkoys <bnwolkoys@gmail.com>
2024-12-05 19:43:27 -0800

    added skill relationship file with variables (#7)
    * added skill relationship file with variables
    
    * removed years in question variable
    
    * changed skill to skillrelationship

---
+++ server/models/Skill_Relationship.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const { Schema } = require("mongoose");
+
+const skillRelationshipSchema = Schema({
+    skill: {
+        type: Schema.Types.ObjectId, 
+        ref: 'Skill',
+    },
+    yearsOfExpereince: {
+        type: Number,
+        required: true,
+    },
+    areasOfExpertise: {
+        type: String,
+    },
+    user: [{ type: Schema.Types.ObjectId, ref: 'User'}]
+});
+
+const SkillRelationship = mongoose.model("SkillRelationship", skillRelationshipSchema);
+
+module.exports = SkillRelationship;
