77f42a8095d831d0f46bb95851bc88121c8885d6
yaseminvatan <66827861+yaseminvatan@users.noreply.github.com>
2024-12-05 20:32:22 -0800

    bug fixed

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -15,8 +15,8 @@ const resolvers = {
     },
   },
   Mutation: {
-    addUser: async (parent, { username, email, password }) => {
-      const user = await User.create({ username, email, password });
+    addUser: async (parent, { username, email, password, location }) => {
+      const user = await User.create({ username, email, password, location });
       const token = signToken(user);
       return { token, user };
     },
