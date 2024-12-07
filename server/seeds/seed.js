40d8309c6c4dd2ca027eb5b4721d316d542dea5c
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-12-06 18:59:48 -0800

    Add skill reset and insert (WIP)

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,7 +1,8 @@
 const db = require("../config/connection");
 const models = require("../models");
-const { User } = require("../models/index");
+const { User, Skill } = require("../models/index");
 const { collection } = require("../models/User");
+const seedSkills = require("./seedSkills.json");
 const seedUsers = require("./seedUsers.json");
 
 const collectionsToReset = [
@@ -9,6 +10,14 @@ const collectionsToReset = [
     modelName: "User",
     collectionName: "users",
   },
+  {
+    modelName: "Skill",
+    collectionName: "skills",
+  },
+  // {
+  //   modelName: "SkillRelationship",
+  //   collectionName: "skillRelationships",
+  // },
 ];
 
 // Collection resetter function
@@ -38,14 +47,48 @@ async function seedCollection({ modelName }, records) {
   }
 }
 
+function mapSkills(user, field, map) {
+  user[field].map((skillNumber) => {
+    return map[skillNumber];
+  });
+}
+
 db.once("open", async () => {
   // Drop all existing collections
   for (const collection of collectionsToReset) {
     await dropCollection(collection);
   }
 
+  // Insert Seed Skills
+  let skillsMap = {};
+  try {
+    const skills = [];
+    seedSkills.map((skill) =>
+      skills.push({ name: skill.name, description: skill.description })
+    );
+    const insertedSkills = await Skill.insertMany(skills);
+
+    const skillNameToIdMap = insertedSkills.reduce(
+      (obj, skill) => Object.assign(obj, { [skill.name]: skill._id }),
+      {}
+    );
+
+    seedSkills.map((skill) => {
+      skillsMap[skill.seedId] = skillNameToIdMap[skill.name];
+    });
+
+    console.log("skillsMap", skillsMap);
+  } catch (err) {
+    throw err;
+  }
+
   // Seed User documents
   try {
+    for (const user of seedUsers) {
+      mapSkills(user, "availableSkills", skillsMap);
+      mapSkills(user, "desiredSkills", skillsMap);
+    }
+
     await User.insertMany(seedUsers);
   } catch (err) {
     throw err;
