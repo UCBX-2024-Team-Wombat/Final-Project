b45329b971937a4188608941562dbaa4dd5c4ced
yaseminvatan <66827861+yaseminvatan@users.noreply.github.com>
2024-12-07 20:23:57 -0800

    Add GraphQL location types  + Query types for fetching (Country, State, City, County) for hierarchical geographic data representation + add location fields to User type

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,10 +1,33 @@
 const typeDefs = `
+  # Location Types
+  type County {
+    name: String!
+  }
+
+  type City {
+    name: String!
+    counties: [County!]!
+  }
+
+  type State {
+    name: String!
+    cities: [City!]!
+  }
+
+  type Country {
+    name: String!
+    states: [State!]!
+  }
+    #User
   type User {
     _id: ID
     username: String
     email: String
     password: String
-    location: Location!
+    city: City!
+    county: County!
+    state: State!
+    country: Country!
   }
   
   type Auth {
@@ -25,21 +48,17 @@ const typeDefs = `
   type Query {
     user(id: ID!): User
     me: User
+    getCountries: [Country!]!
+    getProvinces(countryName: String!): [State!]!
+    getCities(stateName: String!): [City!]!
+    getCounties(cityName: String!): [County!]!
   }
 
   type Mutation {
-    addUser(username: String!, email: String!, password: String!, location: Location!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
   }
-  type Province {
-    name: String!
-  }
-  type Country {
-    name: 
-    city: String!
-    province: Province
-    state: Province
-  }
+ 
 `;
 
 module.exports = typeDefs;
