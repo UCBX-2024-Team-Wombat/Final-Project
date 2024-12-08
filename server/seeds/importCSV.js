7def47594547bb7360ff803308b62ffae89710f8
yaseminvatan <66827861+yaseminvatan@users.noreply.github.com>
2024-12-07 18:59:07 -0800

    Add CSV import functionality for countries, with MongoDB integration.

---
+++ server/seeds/importCSV.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const csv = require('csv-parser'); //csv convertor to JSON // $ npm install csv-parser
+const mongoose = require('mongoose');
+const Country = require('./models/Country'); 
+// burda arraye tanimlamaya ihticac var mi ? sonuclari dondurmek icin ? const results = []  gibi. kaynakta oyle soylemis cunki 
+
+// MongoDB Connection
+mongoose.connect('mongodb://localhost:27017/OUR DATABASE', { // dont forget to write our database name here will we use mine or Jmails or directly cloud ? ideas /
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const importCSV = async () => {
+  const countries = {};
+
+  
