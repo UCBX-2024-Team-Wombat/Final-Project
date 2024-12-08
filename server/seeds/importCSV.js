ca4d09429691ff10152ce0248a03f25e90c60a9d
yaseminvatan <66827861+yaseminvatan@users.noreply.github.com>
2024-12-07 19:00:43 -0800

    we changing the data modelling i delete the files

--- server/seeds/importCSV.js
+++ server/seeds/importCSV.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const csv = require('csv-parser'); //csv convertor to JSON // $ npm install csv-parser
 const mongoose = require('mongoose');
 const Country = require('./models/Country'); 
-// burda arraye tanimlamaya ihticac var mi ? sonuclari dondurmek icin ? const results = []  gibi. kaynakta oyle soylemis cunki 
+// burda arraye tanimlamaya ihticac var mi ? sonuclari dondurmek icin ? const results = []  gibi. kaynakta oyle soylemis cunki ama ben object olarak tanimlamak istiyorum
 
 // MongoDB Connection
 mongoose.connect('mongodb://localhost:27017/OUR DATABASE', { // dont forget to write our database name here will we use mine or Jmails or directly cloud ? ideas /
