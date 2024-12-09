9bdb0ac61ab4e294c59201eaf95b760d0f44de1e
yaseminvatan <66827861+yaseminvatan@users.noreply.github.com>
2024-12-09 05:34:51 -0800

    location feat: Add Location schema with hierarchical fields for city, county, state, and countrySchema

--- server/models/Location.js
+++ server/models/Location.js
@@ -1,7 +1,34 @@
 const mongoose = require("mongoose");
 const { Schema } = require("mongoose");
 
-const locationSchema = new Schema({});
+const locationSchema = new Schema({
+    city: {
+        name: { 
+            type: String,
+            required: true 
+        },
+        county: { 
+            type: String  // Subdocument for City
+        },
+      },
+      state: {
+        name: {
+            type: String 
+        },
+        abbreviation: {
+             type: String //  for additional field
+             }, 
+      },
+      country: {
+        name: { 
+            type: String, 
+            required: true
+        },
+        code: {
+             type: String //Like: 'US'
+            }, 
+      },
+});
 
 const Location = mongoose.model("Location", locationSchema);
 
