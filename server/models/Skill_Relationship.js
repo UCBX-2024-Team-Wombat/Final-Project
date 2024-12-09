e2bda6d717a0906472e68bd353bca4f55e51dc57
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-12-09 13:02:19 -0800

    Remove areaOfExpertise field in favor of specific seeking/offered text fields

--- server/models/Skill_Relationship.js
+++ server/models/Skill_Relationship.js
@@ -9,14 +9,11 @@ const skillRelationshipSchema = Schema({
     yearsOfExperience: {
         type: String,
     },
-    areasOfExpertise: {
-        type: String,
-    },
     offered: Boolean,
     offeredText: String,
-    seeking: Boolean,
-    seekingText: String,
-    user: [{ type: Schema.Types.ObjectId, ref: 'User'}]
+    desired: Boolean,
+    desiredText: String,
+    user: { type: Schema.Types.ObjectId, ref: 'User'}
 });
 
 const SkillRelationship = mongoose.model("SkillRelationship", skillRelationshipSchema);
