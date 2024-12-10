94bf34c5425df4a286cb95624a1ca27ce1c3f38b
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-12-09 17:34:15 -0800

    move Profile.jsx location

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,42 +1,11 @@
 import React from 'react';
 import { useQuery } from "@apollo/client";
-import { QUERY_ME } from '../utils/queries';
-
-//create a base myProfile page (form section)
-// 2 buttons for functionality (current skills, desired skills)
-//autofill
+import { QUERY_ME } from '../../utils/queries';
 
 const Profile = () => {
     const { loading, data } = useQuery(QUERY_ME);
-    // const { loading2, data2 } = useQuery(QUERY_SKILLRELATIONSHIPS);
 
     const userData = data?.me || {};
-
-    // const foundSkillData = data2 || {};
-
-    console.log(userData);
-    // console.log("Skills: ", foundSkillData);
-
-    // const handleChange = (e) => {
-    //     const { name, value } = e.target;
-    //     setUserData({
-    //       ...userData,
-    //       [name]: value,
-    //     });
-    //   };
-    //   const handleSubmit = async (e) => {
-    //     e.preventDefault();
-    //     try {
-    //       await updateUserProfile({
-    //         variables: { ...userData },
-    //       });
-    //       alert('Profile updated!');
-    //     } catch (err) {
-    //       console.error(err);
-    //       alert('Error cannot update profile.');
-    //     }
-    //   };
-
 
     if (loading) {
         return <h2>Loading...</h2>
