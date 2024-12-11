36309ebd90b5e2d556de4a3d1d11fb4d386423a9
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-12-11 14:24:43 -0800

    add placeholder search results component

---
+++ client/src/components/SearchResultsDisplay/SearchResultsDisplay.jsx
@@ -0,0 +1,7 @@
+import Sharer from "../Sharer/Sharer.jsx";
+
+const SearchResultsDisplay = ({ searchPayload }) => {
+  return <>{JSON.stringify(searchPayload)}</>;
+};
+
+export default SearchResultsDisplay;
