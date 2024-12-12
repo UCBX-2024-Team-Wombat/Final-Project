f80fff2b71a140b1a490a7bd3ca01e1d13029c2c
Bradyn Wolkoys <bnwolkoys@gmail.com>
2024-12-12 10:40:06 -0800

    added code to conversation.js file and added it to index.js

--- server/models/index.js
+++ server/models/index.js
@@ -3,7 +3,8 @@ const Rating = require("./Rating.js");
 const Skill = require("./Skill.js");
 const SkillRelationship = require('./Skill_Relationship.js')
 const Message = require('./Message.js') 
+const Conversation = require('./Conversation.js')
 
 // Create model relationships
 
-module.exports = { User, Rating, Skill, SkillRelationship, Message };
+module.exports = { User, Rating, Skill, SkillRelationship, Message, Conversation };
