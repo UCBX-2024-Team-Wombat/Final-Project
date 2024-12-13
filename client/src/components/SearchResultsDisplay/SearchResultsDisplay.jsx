f0b376112840c0a28cfe2072ebf830fc5dcfad4e
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-12-12 17:16:29 -0800

    add "no results" message, modify filtering of incoming data

--- client/src/components/SearchResultsDisplay/SearchResultsDisplay.jsx
+++ client/src/components/SearchResultsDisplay/SearchResultsDisplay.jsx
@@ -3,33 +3,69 @@ import SharerPreview from "../SharerPreview/SharerPreview.jsx";
 const SearchResultsDisplay = ({ searchPayload, showOffered, showDesired }) => {
   const filteredData = () => {
     const filteredData = [];
-    console.log("searchPayload", searchPayload);
 
-    searchPayload?.getSkillRelationshipsBySearchCriteria?.map(
-      (skillRelationship) => {
-        if (skillRelationship.offered && showOffered) {
-          filteredData.push(skillRelationship);
+    Object.keys(searchPayload)?.map((userId) => {
+      const searchRelationshipData = searchPayload[userId].skillRelationships;
+
+      const formattedData = {
+        userInfo: searchPayload[userId].user,
+        offeredSkills: [],
+        desiredSkills: [],
+      };
+
+      for (const relationship of searchRelationshipData) {
+        if (relationship.offered && showOffered) {
+          formattedData.offeredSkills.push(relationship);
         }
-        if (skillRelationship.desired && showDesired) {
-          filteredData.push(skillRelationship);
+        if (relationship.desired && showDesired) {
+          formattedData.desiredSkills.push(relationship);
         }
       }
-    );
+
+      if (
+        (formattedData.offeredSkills.length > 0 && showOffered) ||
+        (formattedData.desiredSkills.length > 0 && showDesired)
+      ) {
+        filteredData.push(formattedData);
+      }
+    });
 
     return filteredData;
   };
 
+  if (!searchPayload) {
+    return <></>;
+  }
+
   return (
     <>
-      {/* {JSON.stringify(filteredData())} */}
+      {filteredData() ? (
+        <div className="fw-bold fs-3">Search Results</div>
+      ) : (
+        <></>
+      )}
       {filteredData().length > 0 ? (
         filteredData().map((data) => {
           return (
             <div key={JSON.stringify(data)}>
-              <SharerPreview payload={data} />;
+              <SharerPreview userInfo={data.userInfo} />
             </div>
           );
         })
+      ) : (
+        <></>
+      )}
+      {filteredData().length == 0 ? (
+        <div className="fw-bold mx-3">
+          <p>
+            Shoot! Unfortunately we weren't able to find any Sharers that
+            matched the criteria you're looking for.
+          </p>
+          <p>
+            You can try again with different skill(s) and/or try your search
+            again with less Sharer filters.
+          </p>
+        </div>
       ) : (
         <></>
       )}
