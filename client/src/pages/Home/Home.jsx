4de5285b65a3587d4f957cacf80aef629ba94cab
Bradyn Wolkoys <bnwolkoys@gmail.com>
2024-12-13 14:20:50 -0800

    added text and image to home screen (#30)

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,7 +1,13 @@
+import myImage from '../../assets/HomeImage.png';
+
 const Home = () => {
   return (
     <>
-      <div>Home placeholder</div>
+      <div>
+      <h1 className="text-center pb-3 mb-2">Welcome to the Skill Share Hub</h1>
+      <p className="text-center pb-3 mb-2">This is a platform designed to facilitate skill exchanges between users. Here, you can trade your skills with others to learn something new. Enjoy!</p>
+      <img className="mx-auto d-block" style={{ width:'400px'}}  src={myImage} alt="thinking/skills image" />
+      </div>
     </>
   );
 };
