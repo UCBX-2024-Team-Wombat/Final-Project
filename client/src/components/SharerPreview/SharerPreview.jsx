3846960a07e2063ac3092f55d2c8277e0ea2766e
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-12-12 17:17:01 -0800

    add skill relationship querying from user id, update skill relationship display

--- client/src/components/SharerPreview/SharerPreview.jsx
+++ client/src/components/SharerPreview/SharerPreview.jsx
@@ -1,7 +1,59 @@
 import { useNavigate } from "react-router-dom";
+import { QUERY_SKILL_RELATIONSHIPS_BY_USER_ID } from "../../utils/queries";
+import { useQuery } from "@apollo/client";
+import { parseSkillRelationshipsByRelationshipType } from "../../utils/dataParsers";
 
-const SharerPreview = ({ payload }) => {
-  const user = payload.user;
+const SharerPreview = ({ userInfo }) => {
+  const user = userInfo;
+
+  const { data: skillRelationships } = useQuery(
+    QUERY_SKILL_RELATIONSHIPS_BY_USER_ID,
+    {
+      variables: {
+        userId: user._id,
+      },
+    }
+  );
+
+  const getParsedSkillRelationships = () =>
+    parseSkillRelationshipsByRelationshipType(
+      skillRelationships?.getSkillRelationshipsByUserId
+    );
+
+  const offeredSkills = () => {
+    return getParsedSkillRelationships()?.offeredSkills;
+  };
+
+  const desiredSkills = () => {
+    return getParsedSkillRelationships()?.desiredSkills;
+  };
+
+  const skillsList = (title, skillsList) => {
+    skillsList.sort((a, b) => {
+      const nameA = a.skill.name.toUpperCase();
+      const nameB = b.skill.name.toUpperCase();
+
+      return nameA > nameB ? 1 : -1;
+    });
+
+    return (
+      <>
+        <div>
+          <div className="fw-bold">{title}</div>
+          <ul>
+            {skillsList.map((skill) => {
+              return (
+                <li key={JSON.stringify(skill)}>
+                  {skill.skill.name} ({skill.yearsOfExperience} year(s))
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </>
+    );
+  };
+
   const navigate = useNavigate();
 
   function viewProfile() {
@@ -10,31 +62,41 @@ const SharerPreview = ({ payload }) => {
 
   return (
     <>
-      <div
-        style={{ border: "1px solid #ccc", padding: "10px", margin: "10px" }}
-      >
-        <h3>{user.username}</h3>
-        <p>
-          <strong> Location:</strong> {user.city}, {user.stateOrProvince},{" "}
-          {user.country}
-        </p>
-        <p>
-          <strong> Gender:</strong> {user.gender}
-        </p>
-        <p>
-          <strong> Meeting Preference:</strong> {user.meetingPreference}
-        </p>
-        <p>
-          <h3> Skills:</h3>
-          <ul>
-            {user.skills?.map((skill) => (
-              <li key={skill.id}>
-                {skill.skill.name} - {skill.ofered ? "Ofered" : "Desired"}
-              </li>
-            ))}
-          </ul>
-        </p>
-        <button onClick={viewProfile}>View Profile</button>
+      <div className="card mb-2">
+        <div className="card-body">
+          <h3 className="card-title">{user.username}</h3>
+          <div className="card-text">
+            <p>
+              <strong> Location:</strong> {user.city}, {user.stateOrProvince},{" "}
+              {user.country}
+            </p>
+            <p>
+              <strong> Gender:</strong> {user.gender}
+            </p>
+            <p>
+              <strong> Meeting Preference:</strong> {user.meetingPreference}
+            </p>
+
+            <div>
+              {offeredSkills() ? (
+                skillsList("Offered Skills", offeredSkills())
+              ) : (
+                <></>
+              )}
+            </div>
+
+            <div>
+              {desiredSkills() ? (
+                skillsList("Desired Skills", desiredSkills())
+              ) : (
+                <></>
+              )}
+            </div>
+          </div>
+          <button className="btn btn-info w-100" onClick={viewProfile}>
+            View Profile
+          </button>
+        </div>
       </div>
     </>
   );
